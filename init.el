f74c1a2d5829bf44066ca593d5679bcb319ececb
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-23 16:28:07 +0100

    Add starting hook to have a buffer which looks like a normal lisp buffer

--- init.el
+++ init.el
@@ -4,8 +4,17 @@
                (add-to-list '*TEMPORARY-PACKAGE-ARCHIVES* '("melpa" . "http://melpa.milkbox.net/packages/") t))
 
 (require 'stumpwm-mode)
+(require 'rainbow-delimiters)
 
 (add-to-list 'auto-mode-alist '("\\.stumpwmrc$" . stumpwm-mode))
+
+(setq stumpwm-mode-hook)
+(add-hook 'stumpwm-mode-hook (lambda ()
+                               (setq major-mode 'lisp-mode)
+                               (lisp-mode-variables)
+                               (paredit-mode 1)
+                               (rainbow-delimiters-mode 1)
+                               (eldoc-mode 1)))
 
 ;; Load bindings config
 (live-load-config-file "bindings.el")
