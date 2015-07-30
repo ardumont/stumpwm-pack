5eaf8ad7de8e2b05dd71e8fb416890c3479d8cf3
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-07-30 22:01:12 +0200

    Use use-package

--- stumpwm-pack.el
+++ stumpwm-pack.el
@@ -4,39 +4,28 @@
 
 ;;; Code:
 
-(require 'install-packages-pack)
-(install-packages-pack/install-packs '(stumpwm-mode
-                                       smartscan))
+(use-package smartscan)
+(use-package rainbow-delimiters)
 
-(require 'stumpwm-mode)
-(add-to-list 'auto-mode-alist '("\\.stumpwmrc$" . stumpwm-mode))
-
-(require 'smartscan)
-(require 'rainbow-delimiters)
-
-(defun stumpwm-pack/find-user-init-file! ()
-  "Find the default user's stumpwmrc file."
-  (interactive)
-  (find-file-other-window "~/.stumpwmrc"))
-
-(global-set-key (kbd "C-c f s") 'stumpwm-pack/find-user-init-file!)
-
-(setq stumpwm-mode-hook)
-(add-hook 'stumpwm-mode-hook
-          (lambda ()
-            (setq major-mode 'lisp-mode)
-            (lisp-mode-variables t t)
-            (paredit-mode 1)
-            (rainbow-delimiters-mode 1)
-            (eldoc-mode 1)
-            (smartscan-mode)
-            (define-key stumpwm-mode-map (kbd "C-c m r") 'stumpwm-eval-region)
-            (define-key stumpwm-mode-map (kbd "C-c m b") 'stumpwm-pack/eval-buffer)))
-
-(defun stumpwm-pack/eval-buffer ()
+(defun stumpwm-pack-eval-buffer ()
   "Eval the current buffer."
   (interactive)
   (stumpwm-eval-region (point-min) (point-max)))
+
+(use-package stumpwm-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.stumpwmrc$" . stumpwm-mode))
+  (add-hook 'stumpwm-mode-hook
+            (lambda ()
+              (setq major-mode 'lisp-mode)
+              (lisp-mode-variables t t)
+              (paredit-mode 1)
+              (rainbow-delimiters-mode 1)
+              (eldoc-mode 1)
+              (smartscan-mode)))
+  :bind (("C-c m r" . stumpwm-eval-region)
+         ("C-c m b" . stumpwm-pack-eval-buffer)))
+
 
 (provide 'stumpwm-pack)
 ;;; stumpwm-pack.el ends here
