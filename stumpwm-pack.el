9dc686df75ca95bd8936cd101c940e4d94dc4bc0
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-11-06 02:35:04 +0100

    stumpwm-pack: Simplify pack

--- stumpwm-pack.el
+++ stumpwm-pack.el
@@ -4,28 +4,27 @@
 
 ;;; Code:
 
-(use-package smartscan)
-(use-package rainbow-delimiters)
+(require 'smartscan)
+(require 'rainbow-delimiters)
 
 (defun stumpwm-pack-eval-buffer ()
   "Eval the current buffer."
   (interactive)
   (stumpwm-eval-region (point-min) (point-max)))
 
-(use-package stumpwm-mode
-  :config
-  (add-to-list 'auto-mode-alist '("\\.stumpwmrc$" . stumpwm-mode))
-  (add-hook 'stumpwm-mode-hook
-            (lambda ()
-              (setq major-mode 'lisp-mode)
-              (lisp-mode-variables t t)
-              (paredit-mode 1)
-              (rainbow-delimiters-mode 1)
-              (eldoc-mode 1)
-              (smartscan-mode)))
-  :bind (("C-c m r" . stumpwm-eval-region)
-         ("C-c m b" . stumpwm-pack-eval-buffer)))
+(require 'stumpwm-mode)
+(add-to-list 'auto-mode-alist '("\\.stumpwmrc$" . stumpwm-mode))
+(add-hook 'stumpwm-mode-hook
+	  (lambda ()
+	    (setq major-mode 'lisp-mode)
+	    (lisp-mode-variables t t)
+	    (paredit-mode 1)
+	    (rainbow-delimiters-mode 1)
+	    (eldoc-mode 1)
+	    (smartscan-mode)))
 
+(define-key stumpwm-mode-map (kbd "C-c m r") 'stumpwm-eval-region)
+(define-key stumpwm-mode-map (kbd "C-c m b") 'stumpwm-pack-eval-buffer)
 
 (provide 'stumpwm-pack)
 ;;; stumpwm-pack.el ends here
